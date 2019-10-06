8eecbe9267229b05a40b961a2ccbfe0d014d681d
EsterMinio <56225762+EsterMinio@users.noreply.github.com>
2019-10-06 17:29:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,21 @@
 import requests  
 import os
 from flask import Flask, request
+import logging
 # Add your telegram token as environment variable
 BOT_URL = f'https://api.telegram.org/bot920184271:AAGf49s0Ju_QYTBPA_HTYOqjSOniAe1qyLg/'
 
 
 app = Flask(__name__)
 
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger('AchicaynaBot')
+
 
 @app.route('/', methods=['POST'])
 def main():  
+    logger.info('Hola')
+
     data = request.json
 
     print(data)  # Comment to hide what Telegram is sending you
